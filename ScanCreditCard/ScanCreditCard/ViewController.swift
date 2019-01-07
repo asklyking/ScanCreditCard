6dd2494640189af4eef7b53971a757c7e50acd70
Sang.TranDuc <macosx@Tran-Duc-Sang.local>
2019-01-07 08:50:02 +0700

    Init project

---
+++ ScanCreditCard/ScanCreditCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanCreditCard
+//
+//  Created by Sang.TranDuc on 1/7/19.
+//  Copyright © 2019 Sang.TranDuc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
