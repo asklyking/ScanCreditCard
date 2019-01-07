5146573b001f1e51c6ddd121efcc1180a4e47abf
Sang.TranDuc <macosx@Tran-Duc-Sang.local>
2019-01-07 18:19:44 +0700

    Add capture image

--- ScanCreditCard/ScanCreditCard/ViewController.swift
+++ ScanCreditCard/ScanCreditCard/ViewController.swift
@@ -9,14 +9,17 @@
 import UIKit
 import AVFoundation
 
-enum LiveMode {
-    case On, Off, Unavailable
-}
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     @IBOutlet weak var btnCapture: UIButton!
     @IBOutlet weak var previewView: UIView!
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer:CALayer!
+    
+    var captureDevice:AVCaptureDevice!
+    
+    var takePhoto = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,8 +28,105 @@ class ViewController: UIViewController {
         btnCapture.layer.borderWidth = 2.0
         btnCapture.layer.borderColor = UIColor.red.cgColor
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        prepareCamera()
+    }
+    
+    
+    func prepareCamera() {
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        if let availableDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices.first {
+            captureDevice = availableDevice
+            beginSession()
+        }
+        
+    }
+    
+    func beginSession () {
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(captureDeviceInput)
+            
+        }catch {
+            print(error.localizedDescription)
+        }
+        
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.previewLayer = previewLayer
+        self.previewView.layer.addSublayer(self.previewLayer)
+        //self.previewLayer.frame = self.previewView.layer.frame
+        DispatchQueue.main.async {
+            self.previewLayer.frame = self.previewView.bounds
+        }
+        captureSession.startRunning()
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString):NSNumber(value:kCVPixelFormatType_32BGRA)] as [String : Any]
+        
+        dataOutput.alwaysDiscardsLateVideoFrames = true
+        
+        if captureSession.canAddOutput(dataOutput) {
+            captureSession.addOutput(dataOutput)
+        }
+        
+        captureSession.commitConfiguration()
+        
+        let queue = DispatchQueue(label: "hust.sangtranduc.captureQueue")
+        dataOutput.setSampleBufferDelegate(self, queue: queue)
+        
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
+        
+        if takePhoto {
+            takePhoto = false
+            
+            if let image = self.getImageFromSampleBuffer(buffer: sampleBuffer) {
+                // process captured image here
+            }
+        }
+    }
+    
+    
+    func getImageFromSampleBuffer (buffer:CMSampleBuffer) -> UIImage? {
+        if let pixelBuffer = CMSampleBufferGetImageBuffer(buffer) {
+            let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+            let context = CIContext()
+            
+            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
+            
+            if let image = context.createCGImage(ciImage, from: imageRect) {
+                return UIImage(cgImage: image, scale: UIScreen.main.scale, orientation: .right)
+            }
+            
+        }
+        
+        return nil
+    }
+    
+    func stopCaptureSession () {
+        self.captureSession.stopRunning()
+        
+        if let inputs = captureSession.inputs as? [AVCaptureDeviceInput] {
+            for input in inputs {
+                self.captureSession.removeInput(input)
+            }
+        }
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
     @IBAction func clickedBtnCapture(_ sender: Any) {
+        takePhoto = true
     }
 }
 
